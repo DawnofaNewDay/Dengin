3395ba38a348ea51f3ac46e952d5d77d3bee32be
Dawn <104056924+DawnofaNewDay@users.noreply.github.com>
2023-03-05 15:44:06 -0800

    Update Player.cs

--- Dengin/GameObjects/Player.cs
+++ Dengin/GameObjects/Player.cs
@@ -17,10 +17,16 @@ public class Player : GameObject
     public override void Move(Vector2f offset)
     {
         Vector2f hypoPos = Pos + offset;
-        if (hypoPos.X + Size.X > _win.Size.X) offset.X = _win.Size.X - (Pos.X + Size.X);
-        if (hypoPos.X < 0) offset.X = -Pos.X;
-        if (hypoPos.Y + Size.Y > _win.Size.Y) offset.Y = _win.Size.Y - (Pos.Y + Size.Y);
-        if (hypoPos.Y < 0) offset.Y = -Pos.Y;
+        
+        if (hypoPos.X + Size.X > _win.Size.X) 
+            offset.X = _win.Size.X - (Pos.X + Size.X);
+        if (hypoPos.X < 0) 
+            offset.X = -Pos.X;
+        if (hypoPos.Y + Size.Y > _win.Size.Y) 
+            offset.Y = _win.Size.Y - (Pos.Y + Size.Y);
+        if (hypoPos.Y < 0) 
+            offset.Y = -Pos.Y;
+            
         base.Move(offset);
     }
 }
